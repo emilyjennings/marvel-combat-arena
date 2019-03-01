812f79f6a372d1f0fd4528c77283a5968d811837
github username <github email address>
2019-03-01 14:33:12 -0500

    added gems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def desc_split(char)
+    char[0]['description'].split(' ')
+  end
+
+  def comic_split(char)
+    char[0]['comics']['items'][0]['name'].split(' ')
+  end
+
+  def desc_blank?(char)
+    char[0]['description'].blank?
+  end
+
+  def first_comic_blank?(char)
+    char[0]['comics']['items'][0].nil?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/play', to: 'characters#play'
+  post '/play', to: 'characters#character_play'
+  post '/searchByLetter', to: 'characters#searchByLetter'
+  root 'characters#play'
+  #I needed to make a way to post these forms to the controller - for instance below I made a post request to the winner method in the characters controller when a player wants to see the winner
+  post '/winner', to: 'characters#winner'
+  #The player will get redirected to a new page to see the winner - so I added that view to the pages in the resource below
+  get '/searchByLetter', to: 'characters#index'
+
+  resources :characters, only: [:index, :winner]
 end
