5e7ff646f6e47701fae11ebcb9f6b7bb7f6ad821
github username <github email address>
2019-03-07 09:03:18 -0500

    still working on making login and signup forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:session][:username])
 
-    if parent && parent.authenticate(params[:session][:password])
-      log_in(parent)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
       redirect_to parent_path(session[:id])
     else
       flash[:notice] = "That didn't work"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+  def log_in(user)
+    session[:id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    current_user ||= User.find_by(id: session[:id])
+
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    current_user != nil
+  end
+
 end
